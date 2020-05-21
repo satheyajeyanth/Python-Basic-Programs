147c7f45d1784f814f72c877f1349e43940c66bc
satheyajeyanth <65716544+satheyajeyanth@users.noreply.github.com>
2020-05-21 18:11:50 +0530

    Add files via upload

---
+++ Basic ATM.py
@@ -0,0 +1,6 @@
+pin = 6666
+user_input = int(input('Enter Your PIN Here: '))
+if(user_input==pin):
+    print("Accepted")
+else:
+    print("PIN Incorrect")
---
+++ basic exam.py
@@ -0,0 +1,10 @@
+mark = 40
+user_input = int(input("enter your mark here:"))
+if (user_input<mark):
+     print ('fail')
+elif (user_input==mark) :
+    print ( 'just pass')
+elif (user_input == 100) :
+    print ( 'full mark')
+else:
+     print ('pass')                  
